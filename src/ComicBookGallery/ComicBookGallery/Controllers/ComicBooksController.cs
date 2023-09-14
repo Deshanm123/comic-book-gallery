c96345763008040dfde3460434b07b23d6856d1e
DEshanm123 <deshanm123@gmail.com>
2023-09-15 00:43:23 +0100

    Implemented Comic book Model ,Controller and View

---
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,54 @@
+﻿using ComicBookGallery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+
+             
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>blah blah spidy</p>",
+                Artists = new Artist[]
+                        {
+                            new Artist
+                            {
+                                Name = "Dan Slott",
+                                Role ="Script"
+
+                            },
+                            new Artist
+                            {
+                                Name = "Humberto Ramos",
+                                Role  ="Pencils"
+                            },
+                            new Artist
+                            {
+                                Name = "Victor Olszba",
+                                Role  ="Inks"
+                            },
+                            new Artist
+                            {
+                                Name = "HChris Elipoulus",
+                                Role  ="Letters"
+                            }
+                            
+                       }
+
+             };
+
+            return View(comicBook); 
+
+        }
+    }
+}
---
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,31 @@
+﻿namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber{ get; set; }
+        public string DescriptionHtml { get; set; } 
+        public Artist[] Artists { get; set; }  
+        public bool favorite { get; set; }
+
+        //This property will be readonly(inother words value cannot be changed by a setter,since no setter
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber;
+            }
+        }
+
+
+        //series-title-issuNumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
+    }
+}
