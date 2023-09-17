82c9a20155d77508958f9ccb2a6bf03c58f3230b
DEshanm123 <deshanm123@gmail.com>
2023-09-17 13:41:41 +0100

    Comic Book repository and view Update

--- src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -34,6 +34,23 @@ namespace ComicBookGallery.Data
                                  },
                         Favorite = false
                  },
+
+                new ComicBook()
+                {
+                    Id = 3,
+                    SeriesTitle = "Bone",
+                    IssueNumber = 50,
+                    DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                    Artists = new Artist[]
+                            {
+                                    new Artist{ Name = "Dan Slott",Role ="Script" },
+                                    new Artist{ Name = "Humberto Ramos",Role  ="Pencils"},
+                                    new Artist{ Name = "Victor Olszba", Role  ="Inks"},
+                                    new Artist{ Name = "HChris Elipoulus", Role  ="Letters"}
+                                },
+                    Favorite = false
+                }
+
         };
         
 
